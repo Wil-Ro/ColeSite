e95c4cff73db1b33f92238f3813d54594922b110
Nye Evans <nyeevans@outlook.com>
2022-04-29 17:15:58 +0100

    filling in empty spaces in draft text

--- scripts/contactme.js
+++ scripts/contactme.js
@@ -3,7 +3,7 @@ function toggleContact(button)
     if (button.innerHTML == "contact me")
     {
         button.innerHTML = "back"
-        document.getElementById("contactBar").style = "font-size: 1rem;"
+        document.getElementById("contactBar").style = "font-size: 1.5rem;"
     }
     else
     {
--- scripts/titlePageLoadAnim.js
+++ scripts/titlePageLoadAnim.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
     document.getElementById("title").style = "margin-top: 40vh;"
-    document.getElementById("menu").style = "font-size: 1rem;"
+    document.getElementById("menu").style = "font-size: 1.8rem;"
 }, 2500); 
